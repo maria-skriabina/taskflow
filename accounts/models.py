2a7514a47c5efdb3c993151643af72a5bb05cf07
Маша Скрябина <gomezpank@gmail.com>
2025-12-13 14:17:08 +0300

    feat(accounts): добавила приложение accounts с моделью UserProfile и миграциями

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    """
+    Кастомная модель пользователя.
+    Наследуемся от стандартной AbstractUser и добавляем свои поля.
+    """
+    phone = models.CharField(
+        max_length=20,
+        verbose_name='Телефон',
+        blank=True,
+        null=True
+    )
+    avatar = models.ImageField(
+        upload_to='avatars/',
+        verbose_name='Аватар',
+        blank=True,
+        null=True
+    )
+    bio = models.TextField(
+        verbose_name='О себе',
+        blank=True,
+        null=True,
+        max_length=500
+    )
+    
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+    
+    def __str__(self):
+        return self.username
--- taskflow_project/settings.py
+++ taskflow_project/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'taskflow_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # для разработки
+
+# Медиа файлы (загружаемые пользователями)
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
