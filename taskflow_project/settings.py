7eb157fc88dd9e71b9f259c6cb66f7dfaebbdab8
maria-skriabina <gomezpank@gmail.com>
2025-12-18 17:28:03 +0300

    config: обновила настройки аутентификации и медиафайлов

--- taskflow_project/settings.py
+++ taskflow_project/settings.py
@@ -128,3 +128,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'accounts.User'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_REDIRECT_URL = '/accounts/profile/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
+LOGIN_URL = '/accounts/login/'
+
