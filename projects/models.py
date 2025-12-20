1810158afca49ea08d63ecb984d0a8683540df18
Luba <lubalev08243@gmail.com>
2025-12-20 17:05:05 +0300

    feat(projects): добавлена модель Project

---
+++ projects/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.conf import settings
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    color = models.CharField(max_length=50, blank=True)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='projects'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
